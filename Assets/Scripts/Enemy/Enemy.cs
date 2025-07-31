bb80b2d06becaae39e479dbe0b7099a07f92da5f
Alex Rodriguez <alex.rodriguez6@g.austincc.edu>
2025-07-30 23:05:02 -0500

    Added animation

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,16 +4,19 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     private NavMeshAgent agent;
+    private Animator enemyAnimation;
     [SerializeField] private Transform endPoint;
+    [SerializeField] private string walkingBool = "IsWalking";
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        enemyAnimation = GetComponent<Animator>();
     }
 
     void Start()
     {
-        
+        enemyAnimation.SetBool(walkingBool, true);
     }
 
     public void Initialized(Transform inputEndPoint)
@@ -25,6 +28,13 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            if (!agent.hasPath || agent.pathStatus == NavMeshPathStatus.PathComplete)
+            {
+                enemyAnimation.SetBool(walkingBool, false);
+            }
+        }
 
     }
 }
